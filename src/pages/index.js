429502879e7de290a9039f95141341aea0083fc6
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2020-03-25 22:48:15 +0400

    filter draft posts from the main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,10 @@ const BlogIndex = ({ data, location }) => {
 
 export const pageQuery = graphql`
   query {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { draft: { ne: true } } }
+    ) {
       edges {
         node {
           excerpt
