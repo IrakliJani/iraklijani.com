3ccd1248972eb554df183f7ed246adb595766523
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2020-03-26 00:48:50 +0400

    updated createPages logic to include navigation between published nodes

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,10 +8,7 @@ exports.createPages = async ({ graphql, actions }) => {
   const result = await graphql(
     `
       {
-        allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
+        allMarkdownRemark(sort: { fields: [frontmatter___date], order: ASC }) {
           edges {
             node {
               fields {
@@ -19,12 +16,13 @@ exports.createPages = async ({ graphql, actions }) => {
               }
               frontmatter {
                 title
+                published
               }
             }
           }
         }
       }
-    `
+    `,
   )
 
   if (result.errors) {
@@ -35,19 +33,29 @@ exports.createPages = async ({ graphql, actions }) => {
   const posts = result.data.allMarkdownRemark.edges
 
   posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node
-    const next = index === 0 ? null : posts[index - 1].node
-
     createPage({
       path: post.node.fields.slug,
       component: blogPost,
       context: {
         slug: post.node.fields.slug,
-        previous,
-        next,
+        previous: findPublishedNode(posts, index, -1),
+        next: findPublishedNode(posts, index, 1),
       },
     })
   })
+}
+
+const findPublishedNode = (posts, currentPostIndex, movement) => {
+  const index = currentPostIndex + movement
+  const post = posts[index]
+
+  if (!post) {
+    return null
+  } else if (post.node.frontmatter.published) {
+    return post
+  } else {
+    return findPublishedNode(posts, index, movement)
+  }
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
